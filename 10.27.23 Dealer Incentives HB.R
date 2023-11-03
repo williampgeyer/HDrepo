7fe84f5758d0df6a4ea49811dc2fd81e9d40e729
Henry Belton <hbelton@marsandco.com>
2023-11-03 18:20:33 -0400

    Other R Pulls

--- 10.27.23 Dealer Incentives HB.R
+++ 10.27.23 Dealer Incentives HB.R
@@ -336,4 +336,193 @@ dt.dealer_avgcostfactor_final_over100[Avg_CF_Index < 5,.(SD = sd(Avg_CF_Index))]
 dt.dealer_avgcostfactor_final_over100[Booking_Size_Group == "High" & Avg_CF_Index < 5,.(SD = sd(Avg_CF_Index))]
 
 
+#### excluding those with < 1000 sales in 2023 ####
 
+dt.dealer_avgcost_by_dealer_weighted_500 = dt.dealers_costfactor_groupings_excladvertising_finite[,list(Booking_2023=sum(Booking_2023),Booking_2022=sum(Booking_2022),
+                                                                                                    MSRP_23=sum(MSRP_23),Avg_CF_Index=weighted.mean(CF_Index,Booking_2023)),by=c("Purchase_Zip","Store_Name")]
+dt.Pop_by_Zip = dt.dealers_costfactor_groupings_excladvertising_finite[,list(Purchase_Zip,Pop_2021,ZCTA,home_value_group,Grouping)][!duplicated(Purchase_Zip)]
+dt.dealer_avgcost_by_dealer_weighted_500 = merge(dt.dealer_avgcost_by_dealer_weighted_500,dt.Pop_by_Zip,all = T)
+
+dt.dealer_avgcost_by_dealer_weighted_500 = dt.dealer_avgcost_by_dealer_weighted_500[Booking_2023 > 1000]
+
+dt.dealer_avgcost_by_dealer_weighted_500[,Booking_Size_Group := fn.cut2_wtd(Booking_2023,Pop_2021,3)]
+summary(dt.dealer_avgcost_by_dealer_weighted_500$Booking_Size_Group)
+dt.dealer_avgcost_by_dealer_weighted_500[Booking_Size_Group == "[    1003,    9773)", Booking_Size_Group := "Low"][Booking_Size_Group == "[    9773,   52023)", Booking_Size_Group := "Medium"][Booking_Size_Group == "[   52023,38675548]", Booking_Size_Group := "High"]
+
+dt.dealer_avgcost_by_dealer_weighted_500[,Booking_Growth := Booking_2023/Booking_2022 - 1]
+
+dt.dealer_avgcost_by_dealer_weighted_500[,Booking_Growth_Group := fn.cut2_wtd(Booking_Growth,Pop_2021,3)]
+summary(dt.dealer_avgcost_by_dealer_weighted_500$Booking_Growth_Group)
+dt.dealer_avgcost_by_dealer_weighted_500[Booking_Growth_Group == "[ -0.996, -0.238)", Booking_Growth_Group := "Low"][Booking_Growth_Group == "[ -0.238,  0.188)", Booking_Growth_Group := "Medium"][Booking_Growth_Group == "[  0.188,126.369]", Booking_Growth_Group := "High"]
+
+## Breaking the CF into segments
+summary(dt.dealer_avgcost_by_dealer_weighted_500$Avg_CF_Index)
+intervals <- c(0,0.9,1.1,150000)
+dt.dealer_avgcost_by_dealer_weighted_500[,Avg_Range := cut(Avg_CF_Index, breaks = intervals, labels = c("Below Avg", "Avg. Range", "Above Avg."))]
+summary(dt.dealer_avgcost_by_dealer_weighted_500$Avg_Range)
+
+dt.dealer_avgcostfactor_final_500 = dt.dealer_avgcost_by_dealer_weighted_500
+
+summary(dt.dealer_avgcostfactor_final_500[Booking_Size_Group == "Low"])
+
+dt.dealer_avgcostfactor_final_500[Avg_CF_Index < 5,.(SD = sd(Avg_CF_Index))]
+dt.dealer_avgcostfactor_final_500[home_value_group == "High" & Avg_CF_Index < 5,.(SD = sd(Avg_CF_Index))]
+
+
+
+
+
+
+
+
+
+
+
+
+#### Margin Analysis ####
+
+dt.dealers_margin = dt.dealers_products_excluding0_2023[,list(Store_Name,Purchase_Zip,Product_Lvl4,Motorization,Booking_2023,Client_Ext_23)]
+dt.dealers_margin_quick = dt.dealers_margin[,list(Booking_2023=sum(Booking_2023),Client_Ext_23=sum(Client_Ext_23)),by=c("Store_Name","Purchase_Zip")]
+dt.dealers_margin_quick[, Margin := (Client_Ext_23 - Booking_2023) / Client_Ext_23]
+dt.margin_noclientext = dt.dealers_margin_quick
+dt.margin_noclientext[Client_Ext_23 == 0,sum(Booking_2023)]
+dt.dealers_margin_quick = dt.dealers_margin_quick[Client_Ext_23 > 0]
+
+dt.dealers_margin_quick[is.na(Margin)]
+
+summary(dt.dealers_margin_quick$Margin)
+intervals <- c(-55701,0,.25,.5,.75,3000)
+dt.dealers_margin_quick[,Avg_Range := cut(Margin, breaks = intervals, labels = c("Well Below Avg", "Below", "About Average","Above Average","Well Above Average"))]
+summary(dt.dealers_margin_quick)
+
+dt.dealers_margin_quick[,sum(Booking_2023),by="Avg_Range"]
+dt.dealers_margin_quick[Avg_Range == "Well Above Average",sum(Booking_2023)]
+dt.dealers_margin_quick[c]
+dt.total_margin = dt.dealers_margin[,list(Booking_2023=sum(Booking_2023),Client_Ext_23=sum(Client_Ext_23))]
+dt.total_margin[, Margin := (Client_Ext_23 - Booking_2023) / Client_Ext_23]
+dt.total_margin_removezeores = dt.dealers_margin[,list(Booking_2023=sum(Booking_2023),Client_Ext_23=sum(Client_Ext_23)),by=c("Store_Name","Purchase_Zip")]
+dt.total_margin_removezeores = dt.total_margin_removezeores[Client_Ext_23 > 0]
+dt.total_margin_removezeores = dt.total_margin_removezeores[,list(Booking_2023=sum(Booking_2023),Client_Ext_23=sum(Client_Ext_23))]
+dt.total_margin_removezeores[, Margin := (Client_Ext_23 - Booking_2023) / Client_Ext_23]
+
+dt.dealerlocations_zip = dt.dealers_margin_quick[,list(Store_Name,Purchase_Zip)]
+
+dt.dealerlocations_zip
+write.csv(dt.dealerlocations_zip,"C:/Users/hbelton/OneDrive - Mars & Co Consulting LLC/Documents/Hunter Douglas/Tableau Overall Metrics/Dealers and Locations.csv")
+
+
+#### Example Dealer Data Issues ####
+
+dt.data_issues = data.table(read.csv("C:/Users/hbelton/Downloads/Data Issues Check_data.csv"))
+
+setnames(dt.data_issues,old=colnames(dt.data_issues),new=c("Store_Name","Purchase_Zip","Measure_Name",
+                                                         "Measure_Values","Booking_Amt","Booking_Units"))
+dt.data_issues = dt.data_issues[,list(`Store_Name`,`Purchase_Zip`,`Measure_Name`,`Measure_Values`)]
+
+dt.data_issues = dcast(dt.data_issues, Store_Name + Purchase_Zip
+                     ~ Measure_Name, value.var = "Measure_Values")
+colnames(dt.data_issues)
+setnames(dt.data_issues,old=colnames(dt.data_issues),new=c("Store_Name","Purchase_Zip","Booking_2023",
+                                                         "Client_Ext_23","Booking_Units_23","Effective_Cost_Factor","Std_cost_23","MSRP_23"))
+sum(dt.data_issues$Booking_2023, na.rm = TRUE)
+
+dt.data_issues_check = dt.data_issues[,list(Store_Name,Purchase_Zip,Booking_2023,Client_Ext_23,MSRP_23)]
+
+dt.data_issues_check = dt.data_issues_check[Booking_2023 > 100000][order(-Booking_2023)][, Dealer_Margin := (Client_Ext_23 - Booking_2023) / Client_Ext_23]
+dt.data_issues_check = dt.data_issues_check[Dealer_Margin < 0]
+
+
+#### Dealer Locations Number of Spots ####
+dt.dealerlocations_zip_ownercount = data.table(read.csv("C:/Users/hbelton/Downloads/Dealer Locations Count_data.csv"))
+dt.dealerlocations_zip_ownercount[,N := 1]
+setnames(dt.dealerlocations_zip_ownercount,old=colnames(dt.dealerlocations_zip_ownercount),new=c("Company","Store_Name","Purchase_Zip","Booking_2023","Count"))
+dt.dealerlocations_zip_ownercount[Company == "HD Brand" & Booking_2023 == 0,sum(Count)]
+length(unique(dt.dealerlocations_zip_ownercount$Store_Name))
+
+write.csv(dt.dealerlocations_zip_ownercount,"C:/Users/hbelton/OneDrive - Mars & Co Consulting LLC/Documents/Hunter Douglas/Tableau Overall Metrics/Dealers and Locations.csv")
+
+#### Dealer Ownership Rollup #### 
+dt.dealerlocations_owner_1 = data.table(read.csv("C:/Users/hbelton/Downloads/Owner Rollup_data (1).csv"))
+setnames(dt.dealerlocations_owner_1,old=colnames(dt.dealerlocations_owner_1),new=c("Owner","Store_Name","Booking_2023"))
+dt.dealerlocations_owner_2 = data.table(read.csv("C:/Users/hbelton/Downloads/Owner Rollup_data (2).csv"))
+setnames(dt.dealerlocations_owner_2,old=colnames(dt.dealerlocations_owner_2),new=c("Owner","Store_Name","Booking_2022"))
+dt.dealerlocations_owner = merge(dt.dealerlocations_owner_1,dt.dealerlocations_owner_2,by=c("Owner","Store_Name"),all = TRUE)
+dt.dealerlocations_owner[, N := 1]
+dt.dealerlocations_owner_count = dt.dealerlocations_owner[,list(Booking_2023=sum(Booking_2023,na.rm = TRUE),Booking_2022=sum(Booking_2022,na.rm = TRUE),N=sum(N,na.rm = TRUE)),by="Owner"][order(-N)]
+
+intervals <- c(0,1,100,1300)
+dt.dealerlocations_owner_count[,Store_Count := cut(N, breaks = intervals, labels = c("One", "Two to 100", "More than 100"))]
+summary(dt.dealerlocations_owner_count)
+dt.dealerlocations_owner_count[Store_Count == "More than 100"]
+dt.dealerlocations_owner_count = dt.dealerlocations_owner_count[Booking_2022>0][Booking_2023<0,Booking_2023:=0][,Booking_Growth := Booking_2023/Booking_2022 - 1]
+dt.dealerlocations_owner_count[, N2 := 1]
+intervals <- c(0,-5,0,5,100000)
+dt.dealerlocations_owner_count[,Growth_Group := cut(N, breaks = intervals, labels = c("Less than -5", "-5 to flat", "flat to 5","Greater than 5"))]
+dt.dealerlocations_owner_groups = dt.dealerlocations_owner_count[,list(Booking_2023=sum(Booking_2023),Booking_2022=sum(Booking_2022),Stores=sum(N),Owners=sum(N2)),by="Store_Count"]
+write.csv(dt.dealerlocations_owner_groups,"C:/Users/hbelton/OneDrive - Mars & Co Consulting LLC/Documents/Hunter Douglas/Tableau Overall Metrics/Ownership Store Count.csv")
+
+intervals <- c(-100000,-5,0,5,1000000)
+dt.dealerlocations_owner_count[,Growth_Group := cut(N, breaks = intervals, labels = c("Less than 5", "-5 to 0", "0 to 5","Greater than 5"))]
+
+dt.dealerlocations_owner_count_rolled_to_owners = 
+  
+#### Dealer Ownership Rollup by Customer ID ####
+dt.dealerlocations_owner_1_ID = data.table(read.csv("C:/Users/hbelton/Downloads/Owner Rollup_data (3).csv"))
+setnames(dt.dealerlocations_owner_1_ID,old=colnames(dt.dealerlocations_owner_1_ID),new=c("Owner","ID","Store_Name","Booking_2023"))
+dt.dealerlocations_owner_2_ID = data.table(read.csv("C:/Users/hbelton/Downloads/Owner Rollup_data (4).csv"))
+setnames(dt.dealerlocations_owner_2_ID,old=colnames(dt.dealerlocations_owner_2_ID),new=c("Owner","ID","Store_Name","Booking_2022"))
+dt.dealerlocations_owner_ID = merge(dt.dealerlocations_owner_1_ID,dt.dealerlocations_owner_2_ID,by=c("Owner","ID","Store_Name"),all = TRUE)
+dt.dealerlocations_owner_ID[, N := 1]
+dt.dealerlocations_owner_count_ID = dt.dealerlocations_owner_ID[,list(Booking_2023=sum(Booking_2023,na.rm = TRUE),Booking_2022=sum(Booking_2022,na.rm = TRUE),N=sum(N,na.rm = TRUE)),by="ID"][order(-N)]
+
+intervals <- c(0,1,100,1300)
+dt.dealerlocations_owner_count_ID[,Store_Count := cut(N, breaks = intervals, labels = c("One", "Two to 100", "More than 100"))]
+summary(dt.dealerlocations_owner_count_ID)
+dt.dealerlocations_owner_count_ID[Store_Count == "Two to 100"]
+dt.dealerlocations_owner_count_ID = dt.dealerlocations_owner_count_ID[Booking_2022>0][Booking_2023<0,Booking_2023:=0][,Booking_Growth := Booking_2023/Booking_2022 - 1]
+dt.dealerlocations_owner_count_ID[, N2 := 1]
+intervals <- c(-10000,-.05,0,.05,100000)
+dt.dealerlocations_owner_count_ID[,Growth_Group := cut(N, breaks = intervals, labels = c("Less than -5", "-5 to flat", "flat to 5","Greater than 5"))]
+dt.dealerlocations_owner_groups_ID = dt.dealerlocations_owner_count_ID[,list(Booking_2023=sum(Booking_2023),Booking_2022=sum(Booking_2022),Stores=sum(N),Owners=sum(N2)),by="Store_Count"]
+
+intervals <- c(-100000,-.05,0,.05,1000000)
+dt.dealerlocations_owner_count_ID_noNA = dt.dealerlocations_owner_count_ID[complete.cases(Store_Count)]
+dt.dealerlocations_owner_count_ID_noNA[,Growth_Group := cut(Booking_Growth, breaks = intervals, labels = c("Less than 5", "-5 to 0", "0 to 5","Greater than 5"))]
+summary(dt.dealerlocations_owner_count_ID_noNA$Growth_Group)
+dt.dealerlocations_owner_count_ID_noNA_pull = dt.dealerlocations_owner_count_ID_noNA[,list(Booking_2023=sum(Booking_2023),Booking_2022=sum(Booking_2022)),by="Growth_Group"]
+dt.dealerlocations_owner_count_ID_Over100 = dt.dealerlocations_owner_count_ID_noNA[Store_Count == "More than 100"][,list(.N,Booking_2023=sum(Booking_2023),Booking_2022=sum(Booking_2022)),by="Growth_Group"]
+
+write.csv(dt.dealerlocations_owner_count_ID_Over100,"C:/Users/hbelton/OneDrive - Mars & Co Consulting LLC/Documents/Hunter Douglas/Tableau Overall Metrics/Ownership Store Count Over 100.csv")
+
+
+
+
+
+
+
+
+dt.dealers_highvslowgrowth = data.table(read.csv("C:/Users/hbelton/Downloads/Unified External Data v7 (1).csv"))
+dt.dealers_highvslowgrowth = dt.dealers_highvslowgrowth[,list(X,ZCTA,Zip.Code,Median_Household_Income,Median_Age_Total,Pop_2021,Pop_2018,Own_vs_rent_own_Perc,Median_census_home_value,households)]
+setnames(dt.dealers_highvslowgrowth,old=colnames(dt.dealers_highvslowgrowth),new=c("X","ZCTA","Purchase_Zip","HH_Income","Age","Pop_2021","Pop_2018","Own_Perc","Home_Value","Households"))
+
+intervals <- c(-10000,-.05,0,.05,100000)
+dt.dealer_avgcost_by_dealer_weighted[,Booking_Growth_Group := cut(Booking_Growth, breaks = intervals, labels = c("Less than -5", "-5 to flat", "flat to 5","Greater than 5"))]
+
+dt.dealer_density = data.table(read.csv("C:/Users/hbelton/Downloads/henry.csv"))
+dt.dealer_density = dt.dealer_density[,list(Purchase_Zip = purchasezip, Dealers_per_10k_HHs_bucket)]
+
+dt.dealers_highvslowgrowth = merge(dt.dealer_avgcost_by_dealer_weighted,dt.dealers_highvslowgrowth,by=c("Purchase_Zip"))
+dt.dealers_highvslowgrowth = dt.dealers_highvslowgrowth[,list(Purchase_Zip,Store_Name,Booking_2023,Pop_2021=Pop_2021.x,Booking_Growth,Booking_Growth_Group,HH_Income,Age,Pop_2018,Own_Perc,Home_Value)]
+dt.dealer_density = dt.dealer_density[!duplicated(Purchase_Zip)]
+dt.dealers_highvslowgrowth = merge(dt.dealers_highvslowgrowth,dt.dealer_density,by=c("Purchase_Zip"))
+dt.dealers_highgrowth = dt.dealers_highvslowgrowth[Booking_Growth_Group == "Greater than 5"]
+write.csv(dt.dealers_highgrowth,"C:/Users/hbelton/OneDrive - Mars & Co Consulting LLC/Documents/Hunter Douglas/Tableau Overall Metrics/Growth over 5 breakout.csv")
+
+
+dt.dealers_lowgrowth = dt.dealers_highvslowgrowth[Booking_Growth_Group == "Less than -5"]
+write.csv(dt.dealers_lowgrowth,"C:/Users/hbelton/OneDrive - Mars & Co Consulting LLC/Documents/Hunter Douglas/Tableau Overall Metrics/Growth under 5 breakout.csv")
+
+
+
+setnames(dt.dealers_home_value,old=colnames(dt.dealers_home_value),new=c("X","ZCTA","Purchase_Zip","Median_census_home_value","Pop_2021","Grouping","home_value_group"))
+dt.dealers_costfactor_groupings_predrop = merge(dt.dealers_products_cf_index,dt.dealers_home_value,by=c("Purchase_Zip"),all=TRUE)
